13ea5672f75eb11a95166117ef01ffde905e41d2
Nikita Spirin <nikitanikita04062000@gmail.com>
2022-03-30 22:10:30 +0300

    Class SettingsActivity initializated

--- Source/app/src/main/java/com/example/dreamsx/SettingsActivity.kt
+++ Source/app/src/main/java/com/example/dreamsx/SettingsActivity.kt
@@ -1,13 +1,25 @@
 package com.example.dreamsx
 
 import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.DatePickerDialog
+import android.app.PendingIntent
+import android.app.TimePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.text.format.DateFormat
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
-class SettingsActivity : AppCompatActivity() {
-    @SuppressLint("RestrictedApi")
+class SettingsActivity : AppCompatActivity() , DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+    @SuppressLint("RestrictedApi", "UseSwitchCompatOrMaterialCode", "UnspecifiedImmutableFlag")
+
+    var hour = 0
+    var minutes = 0
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -15,4 +27,8 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.title = "Настройки"
 
     }
+
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {}
 }
