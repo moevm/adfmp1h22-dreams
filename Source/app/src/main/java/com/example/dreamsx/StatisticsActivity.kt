10f9710b3fe2a159758836695aaada158e0568bc
Orion4ick <42908245+Orion4ick@users.noreply.github.com>
2022-04-05 00:29:25 +0300

    upd

--- Source/app/src/main/java/com/example/dreamsx/StatisticsActivity.kt
+++ Source/app/src/main/java/com/example/dreamsx/StatisticsActivity.kt
@@ -15,6 +15,11 @@ class StatisticsActivity() : AppCompatActivity() {
     lateinit var countOfPositiveDreamsTitle : TextView
     lateinit var countOfMiddleDreamsTitle : TextView
     lateinit var countOfNegativeDreamsTitle : TextView
+    lateinit var topTagsTitle : TextView // топ тегов
+
+    lateinit var weekBtn : Button
+    lateinit var monthBtn : Button
+    lateinit var yearBtn : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,18 +32,25 @@ class StatisticsActivity() : AppCompatActivity() {
         val countOfPositiveDreams = intent.getStringExtra("countOfPositiveDreams")
         val countOfMiddleDreams = intent.getStringExtra("countOfMiddleDreams")
         val countOfNegativeDreams = intent.getStringExtra("countOfNegativeDreams")
-        val arrayOfTags: Array<String> = intent.getStringArrayExtra("arrayOfTags") as Array<String> //Теги 
+        val arrayOfTags: Array<String> = intent.getStringArrayExtra("arrayOfTags") as Array<String> //Теги
 
-
+        weekBtn = findViewById<Button>(R.id.buttonWeek)
+        monthBtn = findViewById<Button>(R.id.buttonMonth)
+        yearBtn = findViewById<Button>(R.id.buttonYear)
         countOfDreamTitle = findViewById<TextView>(R.id.textDreamsTotalCount)
         countOfPositiveDreamsTitle = findViewById<TextView>(R.id.textPositiveDreamsCount)
         countOfMiddleDreamsTitle = findViewById<TextView>(R.id.textMiddleDreamsCount)
         countOfNegativeDreamsTitle = findViewById<TextView>(R.id.textNegativeDreamsCount)
+        topTagsTitle = findViewById<TextView>(R.id.tagsList)
 
         countOfDreamTitle.text = countOfDreams
         countOfPositiveDreamsTitle.text = countOfPositiveDreams
         countOfMiddleDreamsTitle.text = countOfMiddleDreams
         countOfNegativeDreamsTitle.text = countOfNegativeDreams
 
+
+
     }
+
+
 }
