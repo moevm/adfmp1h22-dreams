beb9aeefd93ad1f74f7a7ff1c019eb5fe1f5597c
Nikita Spirin <nikitanikita04062000@gmail.com>
2022-04-15 14:09:37 +0300

    Set the notification's tap action

--- app/src/main/java/com/example/dreamsx/Notification.kt
+++ app/src/main/java/com/example/dreamsx/Notification.kt
@@ -1,6 +1,7 @@
 package com.example.dreamsx
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -15,10 +16,16 @@ class Notification : BroadcastReceiver()
 {
     override fun onReceive(context: Context, intent: Intent)
     {
+        val activityIntent = Intent(context, AddEditNoteActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, activityIntent, PendingIntent.FLAG_IMMUTABLE)
         val notification = NotificationCompat.Builder(context, channelID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(intent.getStringExtra(titleExtra))
             .setContentText(intent.getStringExtra(messageExtra))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
 
         val  manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
